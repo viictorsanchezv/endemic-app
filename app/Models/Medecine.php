35b63ed91a4e9403910ed9bd45c5a782d2ab6964
viictorsanchezv <ingsanchez423@gmail.com>
2022-09-10 18:30:15 -0400

    first models

---
+++ app/Models/Medecine.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Medecine extends Model
+{
+    use HasFactory;
+}
